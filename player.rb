e80426d441e3249b3ddaa8d31b0ca3739b2b4ad9
Rachel Miles <milesrachel9@gmail.com>
2018-02-12 11:56:29 -0700

    add player.rb with wallet

--- player.rb
+++ player.rb
@@ -1,3 +1,25 @@
-class Player
+class Player #name and wallet
+  def initialize()
+    welcome
+  end
 
+  def welcome
+    puts "   Welcome to   "
+    puts "================="
+    puts "===DPL CASINO==="
+    puts "================="
+    puts "What is your name?"
+      player_name = gets.strip
+      puts "Welcome #{player_name}! How much money do you have?"
+      puts "$:"
+      player_wallet = gets.strip.to_f
+      puts "Excellent. Lets find out if you can increase your $#{player_wallet}!!"
+  end
+
+  def wallet
+    player_wallet = gets.strip.to_f
+    return player_wallet
+  end
 end
+
+player = Player.new
