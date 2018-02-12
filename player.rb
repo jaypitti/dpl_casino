b9fba5a57a7acb7daab7caeb3450b7b7e21554e5
Jandrei Pitti <jp.pitti@yahoo.com>
2018-02-12 15:00:18 -0700

    Player and Wallet

--- player.rb
+++ player.rb
@@ -1,11 +1,10 @@
 require_relative 'wallet'
 
 class Player #name and wallet
-  def initialize()
-    welcome
-  end
 
-  def welcome
+  attr_accessor :wallet
+
+  def initialize()
     puts "   Welcome to   "
     puts "================="
     puts "===DPL CASINO==="
@@ -15,10 +14,23 @@ class Player #name and wallet
     puts "Welcome #{player_name}! How much money do you have?"
     puts "$:"
     amount = gets.strip.to_i
-    wallet = Wallet.new(amount)
+    @wallet = Wallet.new(amount)
+    @player_wallet = @wallet.amount
+    puts @wallet.amount
+    binding.pry
     puts "Excellent. Lets find out if you can increase your $#{@player_wallet}!!"
-
+  end
+  def wallet
+    return @player_wallet
+  end
+  def player_bet(wallet, bet)
+    @player_wallet = wallet -= bet
+    return @player_wallet
+  end
+  def retrieve_earnings(total)
+    @player_wallet = total
+  end
+  def add_to_wallet(amount)
+    @player_wallet += amount
   end
 end
-
-player = Player.new
--- wallet.rb
+++ wallet.rb
@@ -1,16 +1,19 @@
-class Wallet < Player
+class Wallet
   def initialize(amount)
     @wallet = amount
   end
-  def wallet
-    return @wallet
+  def amount
+    @wallet
   end
 
   def remove_from_wallet(amount)
     return @wallet -= amount
   end
-  
+
   def add_to_wallet(amount)
     return @wallet += amount
+  end
+  def set_amount(value)
+    @wallet = value
   end
 end
