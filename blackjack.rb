f43898b296b825b3ece9990b3537170ad93feaa1
Liz House <lizehouse@gmail.com>
2018-02-12 11:41:02 -0700

    blackjack initial commit

---
+++ blackjack.rb
@@ -0,0 +1,32 @@
+require_relative 'deck'
+require_relative 'player' #do I need this?
+#do we need to initiate a new deck for this so we aren't using one that is missing cards?
+
+class Blackjack
+
+  attr_reader :player, :casino
+
+  def initialize(player, casino)
+    @player = player
+    @casino = casino
+    play_blackjack
+  end
+
+  def play_blackjack
+    puts '1) Deal'
+    puts '2) Exit to casino'
+    choice = gets.to_i
+    if choice == 1
+      deal
+    elsif choice == 2
+      @casino.menu
+    else
+      puts "Invalid option"
+    end
+  end
+
+  def deal
+    puts "deal"
+  end
+
+end
