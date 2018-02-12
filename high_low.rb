6dfa0a5d8f0f89e20f8840eaa668f6575c7c3ff8
Mitch Merrell <mitchmerrell88@gmail.com>
2018-02-12 13:24:32 -0700

    pull for player.rb rebase

--- high_low.rb
+++ high_low.rb
@@ -7,7 +7,7 @@
 # X randomize from 1 - 100
 # X compare with higher or lower input
 # X win or lose
-# - subtract/add from/to wallet
+# X subtract/add from/to wallet
 
 require_relative 'player'
 
@@ -21,40 +21,48 @@ class Highlow
     @randonum = rand(1...100)
   end
 
+  def hl_walletbal
+    puts "You have $#{@player.wallet} left."
+  end
+
+  def hl_win
+    puts '-' * 20
+    puts "Congrats! You won!"
+    @player.add_to_wallet(@betamount)
+    puts "#{@betamount} was added to your wallet."
+    @highlow.hl_walletbal
+  end
+
+  def hl_lose
+    puts '-' * 20
+    puts "Looks like you lost."
+    @player.remove_from_wallet(@betamount)
+    puts "#{@betamount} was taken from your wallet."
+    @highlow.hl_walletbal
+  end
+
   def highlow_high
     if @bet_or_not > @randonum
-      puts '-' * 20
-      puts "Congrats! You won!"
-#     ####### += @betamount
-      puts "#{@betamount} was added to your wallet."
-      @player.wallet
+      @highlow.hl_win
+      @highlow.hl_walletbal
     elsif @bet_or_not == @randonum
       puts "A tie! No winner or loser."
     else
-      puts '-' * 20
-      puts "Looks like you lost."
-#     ####### -= @betamount
-      puts "#{@betamount} was taken from your wallet."
-      @player.wallet
+      @highlow.hl_lose
+      @highlow.hl_walletbal
     end
     @highlow.highlow_menu
   end
 
   def highlow_low
     if @bet_or_not < @randonum
-      puts '-' * 20
-      puts "Congrats! You won!"
-#     ####### += @betamount
-      puts "#{@betamount} was added to your wallet."
-      @player.wallet
+      @highlow.hl_win
+      @highlow.hl_walletbal
     elsif @bet_or_not == @randonum
       puts "A tie! No winner or loser."
     else
-      puts '-' * 20
-      puts "Looks like you lost."
-#     ####### -= @betamount
-      puts "#{@betamount} was taken from your wallet."
-      @player.wallet
+      @highlow.hl_lose
+      @highlow.hl_walletbal
     end
     @highlow.highlow_menu
   end
@@ -81,7 +89,7 @@ class Highlow
     when 2
       @highlow.highlow_low
     when 3
-      @player.wallet
+      @highlow.hl_walletbal
     when 4
       puts "Okay, enjoy your stay!"
     else
