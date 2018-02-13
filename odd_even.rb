4890ab49ed1eea085ee31cc97f6791d8d41dfc3e
Rachel Miles <milesrachel9@gmail.com>
2018-02-12 17:25:35 -0700

    fix odd/even bug

--- odd_even.rb
+++ odd_even.rb
@@ -9,6 +9,7 @@ class Odds_evens
 
     def initialize (player)
       @player = player
+      @wallet = player.wallet
       guess
     end
 
@@ -63,10 +64,14 @@ class Odds_evens
     def outcome
       if @evenorodd == @player_guess
         puts "You guessed correct!"
-        @player.wallet += @player_gamble
+        @wallet += @player_gamble
+        puts "You now have $#{@wallet} in your wallet"
+        Odds_evens.new(player)
       else
         puts "Loser"
-        @player.wallet -= @player_gamble
+        @wallet -= @player_gamble
+        puts "You now have $#{@wallet} in your wallet"
+        Odds_evens.new(player)
       end
     end
 
